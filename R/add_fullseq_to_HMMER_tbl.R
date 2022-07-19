2d4e6c11934da3a973de47dcda854f72fc9fb989
currocam <campuzanocurro@gmail.com>
2022-07-19 15:12:13 +0200

    refactor: syntax sugar for add_fullseq

--- R/add_fullseq_to_HMMER_tbl.R
+++ R/add_fullseq_to_HMMER_tbl.R
@@ -5,6 +5,7 @@
 #'   `extract_from_HMMER_data_tbl`.
 #' @param fasta_files A character vector containing either the download urls
 #'   (found in the url.fullfasta column) of the files or the path to the files.
+#' @param N.TRIES An integer with the maximum number of attempts to read/download the file.
 #'
 #' @return An `AnnotatedDataFrame` with a new column with the fasta sequences.
 #' @export
@@ -19,16 +20,23 @@
 #'     extract_from_HMMER_data_tbl() %>%
 #'     add_fullseq_to_HMMER_tbl(phmmer_tbl$fullfasta.url)
 #' fullfasta_HMMER_tbl
-add_fullseq_to_HMMER_tbl <- function(HMMER_tidy_tbl, fasta_files) {
+add_fullseq_to_HMMER_tbl <- function(HMMER_tidy_tbl, fasta_files = NULL, N.TRIES = 2) {
   check_AnnotatedDataFrame(HMMER_tidy_tbl)
+
+  if (is.null(fasta_files)) {
+    fasta_files <-HMMER_tidy_tbl$uuid %>%
+      unique() %>%
+      create_download_url_for_hmmer("fullfasta")
+  }
   if (length(fasta_files) != length(unique(HMMER_tidy_tbl$uuid))) {
     stop("The length of fasta_files must be identical to the number of unique",
     "identifiers (uuid) in HMMER_tidy_tbl.")
   }
   fasta_char <- fasta_files %>%
+    purrr::map_chr(~download_if_url(.x , N.TRIES))%>%
     purrr::map2_dfr(unique(HMMER_tidy_tbl$uuid), .f = ~{
 
-      fasta <- .x %>% get_path__and_read_fasta_file()
+      fasta <- .x %>% Biostrings::readAAStringSet()
       data.frame(
         "hits.fullfasta" = as.character(fasta)) %>%
         dplyr::mutate("uuid" = .y,
@@ -40,12 +48,6 @@ add_fullseq_to_HMMER_tbl <- function(HMMER_tidy_tbl, fasta_files) {
   data <- Biobase::pData(HMMER_tidy_tbl) %>%
     dplyr::full_join(fasta_char,
                      by = c("uuid" = "uuid", "hits.name" = "hits.name"))
-  # seqs_names <- data$hits.name
-  # index_duplicated <- duplicated(seqs_names)
-  # seqs_names[index_duplicated] <- paste0(data$hits.name[index_duplicated],
-  #                                        "_", data$uuid[index_duplicated])
-  # data$hits.fullfasta <- data$hits.fullfasta %>%
-  #   magrittr::set_names(seqs_names)
   Biobase::AnnotatedDataFrame(
     data = data,
     varMetadata = rbind(Biobase::varMetadata(HMMER_tidy_tbl), meta)
@@ -58,18 +60,18 @@ download_fasta_file<- function(fasta_url){
   return(temp)
 }
 
-get_path__and_read_fasta_file <- function(fasta_file){
-  if (file.exists(fasta_file)) {
-    fasta_file %>%
-      Biostrings::readAAStringSet()%>%
-      return()
+download_if_url <- function(path, N.TRIES) {
+  if (file.exists(path)) {
+    return(path)
   }
-  else{
-    fasta<- fasta_file %>%
-      download_fasta_file()%>%
-      Biostrings::readAAStringSet()
-    file.remove(fasta_file)
-    return(fasta)
+  i <- 0
+  while (i<N.TRIES) {
+    i <- i + 1
+    new_path <- tryCatch(download_fasta_file(path),
+                     error = function(e) e)
+    if (!inherits(new_path, "error")) {
+      return(new_path)
+    }
   }
+    stop(path)
 }
-
