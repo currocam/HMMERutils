fda9517851ba5e177f0cbeca9eae3aa4b329f812
currocam <campuzanocurro@gmail.com>
2022-10-24 09:51:06 +0200

    feat: read hmmer

---
+++ R/read_hmmer_from_json.R
@@ -0,0 +1,21 @@
+#' Read the results of a search with HMMER from an json file.
+#'
+#' @param file A a character vector the containing filepaths to
+#'   XML file downloaded from HMMER.
+#'
+#' @return A Data Frame with the results
+#' @export
+#'
+read_hmmer_from_json <- function(file){
+  if (is.null(names(file))) {
+    names(file) <- file
+  }
+  inner_function <- purrr::possibly(
+    otherwise = NULL,
+    .f = function(x){
+      jsonlite::read_json(x) %>%
+      purrr::pluck("results") %>%
+      parse_results_into_tbl()
+      })
+  purrr::map_dfr(.x = file, .id = "file", .f = inner_function)
+}
