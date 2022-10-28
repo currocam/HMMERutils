5369157465b7bb6f3d40d835e63c9e7a5a7ad74f
Juan Emilio Martínez <33232217+Juane99@users.noreply.github.com>
2022-10-28 18:34:28 +0200

    First approach
    He pensado en diferentes formas de hacer el split de las columnas:
    
    - Forma 1: Iterar sobre todo el dataframe creando copias de las filas y volver a iterar sobre todo el dataframe para acceder al elemento correspondiente. Separar lista en columnas.
    
    - Forma 2: Iterar una vez sobre el dataframe e insertar en la posición correspondiente las copias, ya accediendo a su elemento correspondiente. Separar lista en columnas.
    
    - Forma 3:  Iterar una vez sobre el dataframe y añadir las nuevas filas al final del dataframe, ya accediendo a su elemento correspondiente. Separar lista en columnas. Ordenar dataframe si es necesario.
    
    De entre todas las aproximaciones la más eficiente es la última.
    
    Por ahora solo soporta la columna de los dominios.

---
+++ R/extract_from_hmmer.R
@@ -0,0 +1,54 @@
+#' Split a column composed of nested lists into multiple columns.
+#'
+#' @param data Dataframe whose column is going to be splitted.
+#' @param column Column to split.
+#'
+#' @return A DataFrame with columns `column` splitted into
+#' several columns.
+#' @export
+extract_from_hmmer <- function(data, column='hits.domains'){
+
+  # Create copy of data
+  data2 <- data
+  
+  # Coerce to data.frame
+  data2 <- as.data.frame(data2)
+  
+  # Save number of rows
+  n.rows <- nrow(data2)
+  
+  # Initialize list with new column
+  new.column <- list()
+  for (row in c(1:n.rows)){
+    new.column[[length(new.column)+1]] <- list()
+  }
+  
+  # Iterate over each row of dataframe
+  for (row in c(1:n.rows)){
+    
+    # Calculate number of domains/sequences in actual row
+    n.elements <- length(data2[row,column][[1]])
+    
+    # If there are more than one domain/sequence, add them at the end of 
+    # dataframe
+    if (n.elements > 1){ 
+      
+      for (el in c(2:n.elements)){
+        data2[nrow(data2)+1,] <- data2[row,]
+        new.column[[length(new.column)+1]] <- 
+          data2[row,column][[1]][[el]]
+      }
+    }
+    
+    # Access to actual row list
+    new.column[[row]] <- data2[row,column][[1]][[1]]
+  }
+  
+  # Substitute new.column by column name and unnest column list into
+  # multiple columns
+  data2 <- cbind(data2,I(new.column))
+  data2 <- data2 %>% select(-c(hits.domains)) %>% 
+    rename({{column}} := new.column) %>% unnest_wider({{column}})  
+  
+  data2
+}
