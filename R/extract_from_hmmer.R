70136367c628886572bb7625ce35c07fd97b7e9c
Juan Emilio Martínez <33232217+Juane99@users.noreply.github.com>
2022-10-31 18:00:49 +0100

    Pdbs now supported

--- R/extract_from_hmmer.R
+++ R/extract_from_hmmer.R
@@ -11,54 +11,62 @@ extract_from_hmmer <- function(data, column='hits.domains'){
   # Create copy of data
   data2 <- data.frame(data)
   
-  # Save number of rows
-  n.rows <- nrow(data2)
-  
-  # Initialize list with new column
-  new.column <- list()
-  for (row in c(1:n.rows)){
-    new.column[[length(new.column)+1]] <- list()
+  if (column == 'hits.pdbs'){
+    data2 <- data2 %>%
+      tidyr::unnest_wider({{column}},names_sep = ".") %>%
+      select_if(~any(!is.na(.)))
   }
-  
-  # Iterate over each row of dataframe
-  for (row in c(1:n.rows)){
-    
-    # Calculate number of domains/sequences in actual row
-    n.elements <- length(data2[row,column][[1]])
-    
-    # If there are more than one domain/sequence, add them at the end of 
-    # dataframe
-    if (n.elements > 1){ 
-      
-      for (el in c(2:n.elements)){
-        data2[nrow(data2)+1,] <- data2[row,]
-        assigned.element <- data2[row,column][[1]][[el]]
-        
-        if (is.null(assigned.element)){
-          assigned.element <- NA
-        }
-        new.column[[length(new.column)+1]] <- assigned.element
-      }
-    }
-    
-    # Access to actual row list
-    assigned.element <- data2[row,column][[1]][[1]]
-    
-    if (is.null(assigned.element)){
-      assigned.element <- NA
-    }
-    
-    new.column[[row]] <- assigned.element
-  }
-  
-  # Substitute new.column by column name and unnest column list into
-  # multiple columns
-  data2 <- cbind(data2,I(new.column))
+  else{
 
-  data2 <- data2 %>% dplyr::select(-c({column})) %>% 
-    dplyr::rename({{column}} := new.column) %>%
-    tidyr::unnest_wider({{column}},names_sep = ".") %>%
-    select_if(~any(!is.na(.)))
+    # Save number of rows
+    n.rows <- nrow(data2)
+    
+    # Initialize list with new column
+    new.column <- list()
+    for (row in c(1:n.rows)){
+      new.column[[length(new.column)+1]] <- list()
+    }
+    
+    # Iterate over each row of dataframe
+    for (row in c(1:n.rows)){
+      
+      # Calculate number of domains/sequences in actual row
+      n.elements <- length(data2[row,column][[1]])
+      
+      # If there are more than one domain/sequence, add them at the end of 
+      # dataframe
+      if (n.elements > 1){ 
+        
+        for (el in c(2:n.elements)){
+          data2[nrow(data2)+1,] <- data2[row,]
+          assigned.element <- data2[row,column][[1]][[el]]
+          
+          if (is.null(assigned.element)){
+            assigned.element <- NA
+          }
+          new.column[[length(new.column)+1]] <- assigned.element
+        }
+      }
+      
+      # Access to actual row list
+      assigned.element <- data2[row,column][[1]][[1]]
+      
+      if (is.null(assigned.element)){
+        assigned.element <- NA
+      }
+      
+      new.column[[row]] <- assigned.element
+    }
+    
+    # Substitute new.column by column name and unnest column list into
+    # multiple columns
+    data2 <- cbind(data2,I(new.column))
+
+    data2 <- data2 %>% dplyr::select(-c({column})) %>% 
+      dplyr::rename({{column}} := new.column) %>%
+      tidyr::unnest_wider({{column}},names_sep = ".") %>%
+      select_if(~any(!is.na(.)))
+  }
 
   # Remove 'hits.' prefix from colnames
   colnames(data2) <- colnames(data2) %>%
