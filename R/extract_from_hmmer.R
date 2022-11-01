071d2a9cb394b8a43f9b3694a7e3cc23b616a223
Juan Emilio Martínez <33232217+Juane99@users.noreply.github.com>
2022-11-01 15:07:59 +0100

    Function modified, now pdbs are added as new rows

--- R/extract_from_hmmer.R
+++ R/extract_from_hmmer.R
@@ -10,62 +10,64 @@ extract_from_hmmer <- function(data, column='hits.domains'){
   
   # Create copy of data
   data2 <- data.frame(data)
-  
-  if (column == 'hits.pdbs'){
-    data2 <- extract_pdb_from_hmmer(data2)
-  }
-  else{
 
-    # Save number of rows
-    n.rows <- nrow(data2)
+  # Save number of rows
+  n.rows <- nrow(data2)
     
-    # Initialize list with new column
-    new.column <- list()
-    for (row in c(1:n.rows)){
-      new.column[[length(new.column)+1]] <- list()
-    }
+  # Initialize list with new column
+  new.column <- list()
+  for (row in c(1:n.rows)){
+    new.column[[length(new.column)+1]] <- list()
+  }
     
-    # Iterate over each row of dataframe
-    for (row in seq_len(n.rows)){
+  # Iterate over each row of dataframe
+  for (row in seq_len(n.rows)){
       
-      # Calculate number of domains/sequences in actual row
-      n.elements <- length(data2[row,column][[1]])
+    # Calculate number of domains/sequences in actual row
+    n.elements <- length(data2[row,column][[1]])
       
-      # If there are more than one domain/sequence, add them at the end of 
-      # dataframe
-      if (n.elements > 1){ 
+    # If there are more than one domain/sequence, add them at the end of 
+    # dataframe
+    if (n.elements > 1){ 
         
-        for (el in c(2:n.elements)){
-          data2[nrow(data2)+1,] <- data2[row,]
-          assigned.element <- data2[row,column][[1]][[el]]
+      for (el in c(2:n.elements)){
+        data2[nrow(data2)+1,] <- data2[row,]
+        assigned.element <- data2[row,column][[1]][[el]]
           
-          if (is.null(assigned.element)){
-            assigned.element <- NA
-          }
-          new.column[[length(new.column)+1]] <- assigned.element
+        if (is.null(assigned.element)){
+          assigned.element <- NA
         }
+        new.column[[length(new.column)+1]] <- assigned.element
       }
-      
-      # Access to actual row list
-      assigned.element <- data2[row,column][[1]][[1]]
-      
-      if (is.null(assigned.element)){
-        assigned.element <- NA
-      }
-      
-      new.column[[row]] <- assigned.element
     }
-    
-    # Substitute new.column by column name and unnest column list into
-    # multiple columns
-    data2 <- cbind(data2,I(new.column))
-
-    data2 <- data2 %>% dplyr::select(-c({column})) %>% 
-      dplyr::rename({{column}} := new.column) %>%
-      tidyr::unnest_wider({{column}},names_sep = ".") %>%
-      dplyr::select_if(~any(!is.na(.)))
+      
+    # Access to actual row list
+    assigned.element <- data2[row,column][[1]][[1]]
+      
+    if (is.null(assigned.element)){
+      assigned.element <- NA
+    }
+      
+    new.column[[row]] <- assigned.element
   }
+    
+  # Substitute new.column by column name and unnest column list into
+  # multiple columns
+  data2 <- cbind(data2,I(new.column))
 
+  data2 <- data2 %>% 
+  dplyr::select(-c({column})) %>% 
+  dplyr::rename({{column}} := new.column)
+  
+  if (column != 'hits.pdbs'){
+    data2 <- data2 %>% 
+    tidyr::unnest_wider({{column}},names_sep = ".")
+  }
+  
+  # Remove empty columns
+  data2 <- data2 %>% 
+  dplyr::select_if(~any(!is.na(.)))
+  
   # Remove 'hits.' prefix from colnames
   colnames(data2) <- colnames(data2) %>%
     stringr::str_replace_all(
@@ -74,11 +76,4 @@ extract_from_hmmer <- function(data, column='hits.domains'){
   
   # Return new dataframe
   data2
-}
-
-extract_pdb_from_hmmer <- function(data){
-  column <- 'hits.pdbs'
-  data %>%
-    tidyr::unnest_wider({{column}},names_sep = ".") %>%
-    dplyr::select_if(~any(!is.na(.)))
 }
