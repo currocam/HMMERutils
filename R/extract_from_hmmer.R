4d639d14f648eb0d2f776ba6bc4b041c3e35303b
Juan Emilio Martínez <33232217+Juane99@users.noreply.github.com>
2022-11-01 17:39:50 +0100

    Modularized functions to take less than 50 lines

--- R/extract_from_hmmer.R
+++ R/extract_from_hmmer.R
@@ -7,13 +7,10 @@
 #' several columns.
 #' @export
 extract_from_hmmer <- function(data, column='hits.domains'){
-  
   # Create copy of data
   data2 <- data.frame(data)
-
   # Save number of rows
   n.rows <- nrow(data2)
-    
   # Initialize list with new column
   new.column <- list()
   for (row in seq_len(n.rows)){
@@ -22,14 +19,12 @@ extract_from_hmmer <- function(data, column='hits.domains'){
     
   # Iterate over each row of dataframe
   for (row in seq_len(n.rows)){
-      
     # Calculate number of domains/sequences in actual row
     n.elements <- length(data2[row,column][[1]])
       
     # If there are more than one domain/sequence, add them at the end of 
     # dataframe
     if (n.elements > 1){ 
-        
       for (el in c(2:n.elements)){
         data2[nrow(data2)+1,] <- data2[row,]
         assigned.element <- data2[row,column][[1]][[el]]
@@ -40,28 +35,41 @@ extract_from_hmmer <- function(data, column='hits.domains'){
         new.column[[length(new.column)+1]] <- assigned.element
       }
     }
-      
     # Access to actual row list
     assigned.element <- data2[row,column][[1]][[1]]
-      
     if (is.null(assigned.element)){
       assigned.element <- NA
-    }
-      
+    } 
     new.column[[row]] <- assigned.element
   }
-    
   # Substitute new.column by column name and unnest column list into
   # multiple columns
+  data2 <- data2 %>% bind_and_unnest(column,new.column)
+  data2
+}
+
+#' Aux function used to bind and unnest wider the new list column.
+#'
+#' @param data Dataframe whose column is going to be splitted.
+#' @param old.column Column to remove.
+#' @param new.column Column to add.
+#'
+#' @return A DataFrame with columns `column` splitted into
+#' several columns.
+#' @export
+bind_and_unnest <- function(data, old.column, new.column){
+  
+  data2 <- data.frame(data)
+
   data2 <- cbind(data2,I(new.column))
 
   data2 <- data2 %>% 
-  dplyr::select(-c({column})) %>% 
-  dplyr::rename({{column}} := new.column)
+  dplyr::select(-c({old.column})) %>% 
+  dplyr::rename({{old.column}} := new.column)
   
-  if (column != 'hits.pdbs'){
+  if (old.column != 'hits.pdbs'){
     data2 <- data2 %>% 
-    tidyr::unnest_wider({{column}},names_sep = ".")
+    tidyr::unnest_wider({{old.column}},names_sep = ".")
   }
   
   # Remove empty columns
@@ -71,8 +79,8 @@ extract_from_hmmer <- function(data, column='hits.domains'){
   # Remove 'hits.' prefix from colnames
   colnames(data2) <- colnames(data2) %>%
     stringr::str_replace_all(
-      column, 
-      stringr::str_remove(column, "hits."))
+      old.column, 
+      stringr::str_remove(old.column, "hits."))
   
   # Return new dataframe
   data2
