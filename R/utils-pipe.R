6c222c4a9984c7f657a381b5e7a2249c31e7e328
Curro Campuzano <campuzanocurro@gmail.com>
2022-11-05 16:11:10 +0100

    remove unnecesary example

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -11,10 +11,5 @@
 #' @param lhs A value or the magrittr placeholder.
 #' @param rhs A function call using the magrittr semantics.
 #' 
-#' @examples
-#' githubURL <- "https://raw.githubusercontent.com/currocam/HMMERutils/4-extract_from_hmmer/inst/extdata/data_short.rds"
-#' download.file(githubURL,"short_data.rds",method="curl")
-#' data <- readRDS("short_data.rds")
-#' data %>% dplyr::select(tidyselect::starts_with("pdbs"))
 #' @return The result of calling `rhs(lhs)`.
 NULL
