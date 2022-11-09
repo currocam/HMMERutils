ccce37037eb815a4977b9a00eca706a6624038bd
Curro Campuzano <69399781+currocam@users.noreply.github.com>
2022-11-09 10:16:02 +0100

    refactor: remove unnecessary with

--- tests/testthat/test-filter_hmmer.R
+++ tests/testthat/test-filter_hmmer.R
@@ -1,5 +1,4 @@
-httptest::with_mock_api({
-  testthat::test_that("Test that filter hmmer works hits evalue", {
+testthat::test_that("Test that filter hmmer works hits evalue", {
 
     data(phmmer_2abl)
     phmmer_2abl %>%
@@ -7,27 +6,20 @@ httptest::with_mock_api({
       dplyr::select("hits.evalue") %>%
       testthat::expect_snapshot()
   })
-})
-
-
-httptest::with_mock_api({
-  testthat::test_that("Test that filter hmmer works hits domains cevalue", {
+testthat::test_that("Test that filter hmmer works hits domains cevalue", {
 
     data(phmmer_2abl)
     phmmer_2abl %>%
       filter_hmmer(by = "domains.cevalue") %>%
       dplyr::select("domains.cevalue") %>%
       testthat::expect_snapshot()
-  })
 })
 
-httptest::with_mock_api({
-  testthat::test_that("Test that filter hmmer works hits domains ievalue", {
+testthat::test_that("Test that filter hmmer works hits domains ievalue", {
 
     data(phmmer_2abl)
     phmmer_2abl %>%
       filter_hmmer(by = "domains.ievalue") %>%
       dplyr::select("domains.ievalue") %>%
       testthat::expect_snapshot()
-  })
 })
