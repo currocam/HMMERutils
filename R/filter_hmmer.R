fa3c4371b31df1a338f6409a5804d454e1d5e320
Juan Emilio Martínez <33232217+Juane99@users.noreply.github.com>
2022-11-09 11:16:44 +0100

    Code refactor

--- R/filter_hmmer.R
+++ R/filter_hmmer.R
@@ -6,8 +6,6 @@
 #'
 #' @return A DataFrame filtered.
 #' 
-#' @importFrom dplyr filter
-#' 
 #' @examples
 #' data(phmmer_2abl)
 #' filter_hmmer(
@@ -17,60 +15,60 @@
 #' )
 #' @export
 filter_hmmer <- function(data, threshold = 0.0005, by = "hits.evalue"){
-  data2 <- data.frame(data)
-  
-  # Extract type
-  type <- strsplit(by, split = "[.]")[[1]]
-  
-  if (type[1] == "domains"){ # Filtering domains
+    data2 <- data.frame(data)
     
-    if (!(by %in% colnames(data))){ # Lets create column
-      data2 <- data2 %>% extract_evalue_from_domains(by=type[2])
+    # Extract type
+    type <- strsplit(by, split = "[.]")[[1]]
+    
+    if (type[1] == "domains"){ # Filtering domains
+    
+        if (!(by %in% colnames(data))){ # Lets create column
+            data2 <- data2 %>% extract_evalue_from_domains(by=type[2])
+        }
     }
-  }
-  
-  # Filter desired column
-  data2 %>% dplyr::filter(get({{by}}) < threshold)
+    
+    # Filter desired column
+    data2 %>% dplyr::filter(get({{by}}) < threshold)
 }
 
 
 extract_evalue_from_domains <- function(data, by="ievalue"){
-  data2 <- data.frame(data)
+    data2 <- data.frame(data)
   
-  # Create new empty column
-  new_evalue <- c()
+    # Create new empty column
+    new_evalue <- c()
   
-  # Save number of rows
-  n.rows <- nrow(data2)
+    # Save number of rows
+    n.rows <- nrow(data2)
   
-  # Iterate over all rows
-  for (row in seq_len(n.rows)){
+    # Iterate over all rows
+    for (row in seq_len(n.rows)){
     
-    # Calculate number of domains in actual row
-    n.elements <- length(data2[row,'hits.domains'][[1]])
+        # Calculate number of domains in actual row
+        n.elements <- length(data2[row,'hits.domains'][[1]])
     
-    lowest <- as.double(data2[row,'hits.domains'][[1]][[1]][by])
+        lowest <- as.double(data2[row,'hits.domains'][[1]][[1]][by])
     
-    # Iterate over each domain and keep lowest
-    if (n.elements > 1){ 
+        # Iterate over each domain and keep lowest
+        if (n.elements > 1){ 
       
-      for (el in c(1:n.elements)){
+            for (el in seq_len(n.elements)){
 
-        element <- as.double(data2[row,'hits.domains'][[1]][[el]][by])
+                element <- as.double(data2[row,'hits.domains'][[1]][[el]][by])
         
-        if (element < lowest){ # Keep lowest
-          lowest <- element
+                if (element < lowest){ # Keep lowest
+                    lowest <- element
+                }
+        
+            }
         }
-        
-      }
+        # Store new value
+        new_evalue <- c(new_evalue, lowest)
     }
-    # Store new value
-    new_evalue <- c(new_evalue, lowest)
-  }
   
-  # Add new column
-  data2 <- cbind(data2, new_evalue)
-  colnames(data2)[ncol(data2)] <- paste("domains",by,sep=".")
+    # Add new column
+    data2 <- cbind(data2, new_evalue)
+    colnames(data2)[ncol(data2)] <- paste("domains",by,sep=".")
 
-  data2
+    data2
 }
